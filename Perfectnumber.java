9f613d081d7b1c5ce36b22284f7dbf663f48ac0a
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-31 12:57:02 +0530

    Create Perfectnumber.java

---
+++ Perfectnumber.java
@@ -0,0 +1,19 @@
+public class Perfectnumber {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	System.out.println("Enter your Number");
+	int temp=0;
+	int a=in.nextInt();
+	for(int i=1;i<a;i++) {
+		if(a%i==0) {
+			temp=temp+i;
+			System.out.println(i);
+		}
+		
+	}
+	if(temp==a) {
+		System.out.println("Perfect Number"+a);
+	}
+
+}
+}
