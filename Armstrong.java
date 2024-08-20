5735e1539b13e155c6b57e7a6e87f39425832aa5
saipradeep484 <saipradeep484@gmail.com>
2024-08-20 18:26:07 +0530

    assignment

--- Armstrong.java
+++ Armstrong.java
@@ -7,46 +7,33 @@ public class Armstrong {
 	
     public int countDigits(int num) {
 
-    	int count = 0;
-        while (num != 0) {
-//        	153
-//        	divide it until num becomes 0 
-            num = num / 10;
-//            15
-//            add count by every iteration
-            count++;
-        }
+    	String s = String.valueOf(num);
+        int count = s.length();
         return count;
     }
 	public void Arm() {
-        int b;
-        int sum = 0;
+        // int b;
+        // int sum = 0;
         Scanner in = new Scanner(System.in);
         System.out.println("Give your Value");
         int a = in.nextInt();
-        int temp = a;
-        int n = countDigits(a); // Calculate the number of digits in the input number
+         String s = String.valueOf(a);
+        int max = s.length()/2;
+        int count=0;
+        int i=0;
         
-        while (a > 0) {
-            b = a % 10;
-//            initial power to 1
-            int power = 1;
-//            n states about number of numbers in given int
-            for (int i = 1; i <=n; i++) {
-//            	iterations starts from 0 and rasise to <n ex:
-                power *= b;
-//                power*=1*(3*3*3) three iterations if we take 153
-            }
-            sum += power;
-//            adding it it to sum variable
-            a = a / 10;
-//           by dividing it we can again get remianing value; 
-        }
-        if (temp == sum) {
-            System.out.println(temp + " is an Armstrong");
-        } else {
-            System.out.println(temp + " is not an Armstrong");
-        }
+       
+        int j=s.length()-1;
+        while (i<max && (j)>=max) {
+            if(s.charAt(i)==s.charAt(j))
+            count++;
+            i++;
+            j--;
+        }   
+        if(count==max)
+        System.out.println("yes it is Armstrong");
+        else
+        System.out.println("no it is not a armstrong ");
     }
 
     // Method to count the number of digits in a number
@@ -55,5 +42,6 @@ public class Armstrong {
     public static void main(String[] args) {
         Armstrong m = new Armstrong();
         m.Arm();
+        System.out.println(m.countDigits(9987));
     }
 }
--- Perfectnumber.java
+++ Perfectnumber.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Perfectnumber {
 public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
